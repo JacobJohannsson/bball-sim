6485a2b88544459482c6946f06158caa2ff10ec7
JacobJohannsson <jakbrain@gmail.com>
2023-08-07 15:39:31 -0400

    Deleted some extra garbage

--- build/lib/source/basement/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Get the user's home directory (C:/Users/user/ on Windows)
-home_directory = os.path.expanduser("~")
-
-# Specify the directory name to create in the user's home directory
-directory_name = "bball"
-
-INSTALL_PATH = os.path.join(home_directory, directory_name)
